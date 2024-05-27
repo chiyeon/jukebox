8ed95634e9ee4ca7eb02dd8d1805817bb85b7c8d
Benjamin Wong <chiyeonwastaken@gmail.com>
2024-05-27 01:45:37 -0700

    '/upload' endpoint streams files to dropbox storage

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const fs = require("fs")
 const express = require("express")
 const multer = require("multer")
 const ejs = require("ejs")
+const { Dropbox } = require("dropbox")
+const { WritableStreamBuffer } = require("stream-buffers")
 require("dotenv").config()
 
 const app = express()
 const PORT = process.env.PORT | 8080
-const storage = multer.diskStorage({
-   destination: (req, file, cb) => {
-      cb(null, "tracks/")
-   },
-   filename: (req, file, cb) => {
-      console.log(file)
-      cb(null, Date.now() + "_" + file.originalname)
-   }
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+const dbx = new Dropbox({
+   accessToken: process.env.DROPBOX_TOKEN,
+   fetch: fetch
 })
 
 app.set("view engine", "ejs")
@@ -22,9 +21,26 @@ app.get("/", (req, res) => {
    res.render("home", { msg: "hi from ejs" })
 })
 
-app.post("/upload", multer({ storage }).single("file"), (req, res, next) => {
-   console.log("Recieved file")
-   res.sendStatus(200)
+app.post("/upload", upload.single("file"), async (req, res) => {
+   try {
+      const file = req.file
+      const filename = file.originalname
+      const filebuffer = file.buffer
+
+      const bufferstream = new WritableStreamBuffer({
+         initialSize: (100 * 1024),
+         incrementAmount: (10 * 1024)
+      })
+
+      bufferstream.end(filebuffer)
+
+      await dbx.filesUpload({ path: `/${filename}`, contents: bufferstream.getContents() })
+
+      res.status(200).send("Successfully uploaded")
+   } catch (err) {
+      console.log("Error uploading file:", err)
+      res.status(500).send("Error uploading file")
+   }
 })
 
 app.listen(8080, () => console.log("started"))
