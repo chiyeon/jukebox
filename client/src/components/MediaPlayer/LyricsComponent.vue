49990abd158f7e08faff464cb266d7003125c788
Benjamin Wong <chiyeonwastaken@gmail.com>
2024-07-18 01:28:38 -0700

    Improved lyrics page

--- client/src/components/MediaPlayer/LyricsComponent.vue
+++ client/src/components/MediaPlayer/LyricsComponent.vue
@@ -5,9 +5,11 @@
          @click="eventbus.emit('set_lyrics_visibility', false)"
          >keyboard_arrow_down</span
       >
-      <p class="lyrics">
-         {{ lyrics }}
-      </p>
+      <div class="lyrics-scroll">
+         <p class="lyrics">
+            {{ lyrics }}
+         </p>
+      </div>
    </div>
 </template>
 
@@ -25,8 +27,6 @@ defineProps(["lyrics"])
    }
 }
 .lyrics-box {
-   overflow-y: auto;
-
    position: fixed;
    bottom: 0;
    left: 0;
@@ -36,16 +36,57 @@ defineProps(["lyrics"])
    height: 100vh;
    height: 100dvh;
 
-   padding-bottom: 300px;
-
    background-color: white;
    box-sizing: border-box;
 }
+
+.lyrics-scroll {
+   position: relative;
+   height: calc(100% - 265px); /* about the size of the bottom player */
+}
+
+.lyrics-scroll::before,
+.lyrics-scroll::after {
+   content: '';
+   position: absolute;
+   left: 0;
+   right: 0;
+   height: 50px;
+   pointer-events: none;
+   z-index: 1;
+}
+
+.lyrics-scroll::before {
+   top: 0;
+   background: none;
+   background: linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+}
+
+.lyrics-scroll::after {
+   bottom: 0;
+   background: none;
+   background: linear-gradient(to top, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+}
+
 .lyrics {
    white-space: pre-line;
    font-size: 32px;
    font-weight: bold;
    color: cadetblue;
+   overflow-y: auto;
+   height: 100%;
+}
+
+.lyrics::before {
+   content: "";
+   display: block;
+   padding-top: 28px;
+}
+
+.lyrics::after {
+   content: "";
+   display: block;
+   padding-bottom: 300px;
 }
 
 .exit {
@@ -54,6 +95,7 @@ defineProps(["lyrics"])
    background-color: lightcoral;
    cursor: pointer;
    margin-top: 30px;
+   border-radius: 4px;
 }
 
 .exit:hover {
