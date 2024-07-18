04d05b7f17260f09ced1364643f14d9ecf338fc2
Benjamin Wong <chiyeonwastaken@gmail.com>
2024-07-18 01:04:46 -0700

    Large mobile player fits better and scales from the top

--- client/src/components/MediaPlayer/PlayerMobile.vue
+++ client/src/components/MediaPlayer/PlayerMobile.vue
@@ -269,10 +269,11 @@ button {
 
 /* bigt player */
 .big {
+   top: 0;
    --padding: 40px;
    padding: 10px var(--padding) var(--padding) var(--padding);
-   height: 90vh;
-   height: 90vdh;
+   height: 100vh;
+   height: 100vdh;
 }
 
 .mobile_expanded span {
